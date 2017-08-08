37b7e9c470fde3e9567176f3c9682708cf3cf15b
Fhad Sheikh <fhad@getmaple.ca>
2017-08-08 16:45:37 -0400

    use let instead of const

--- components/StatusBarEnhanced.js
+++ components/StatusBarEnhanced.js
@@ -1,51 +1,51 @@
-import React from "react"
-import PropTypes from "prop-types"
-import { Platform, View, StatusBar, ViewPropTypes } from "react-native"
-import styles from "../styles"
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Platform, View, StatusBar, ViewPropTypes } from 'react-native';
+import styles from '../styles';
 
 function StatusBarEnhanced({ statusBar, style }) {
-  const statusBarConfig = {}
-  if (Platform.OS === "ios") {
-    statusBarConfig = {
-      animated: true,
-      hidden: false,
-      barStyle: "default",
-      networkActivityIndicatorVisible: false,
-      showHideTransition: "fade",
+    let statusBarConfig = {};
+    if (Platform.OS === 'ios') {
+        statusBarConfig = {
+            animated: true,
+            hidden: false,
+            barStyle: 'default',
+            networkActivityIndicatorVisible: false,
+            showHideTransition: 'fade'
+        };
+    } else if (Platform.OS === 'android') {
+        statusBarConfig = {
+            animated: true,
+            hidden: false,
+            showHideTransition: 'fade',
+            backgroundColor: 'rgba(0, 0, 0, 0.2)',
+            translucent: true
+        };
     }
-  } else if (Platform.OS === "android") {
-    statusBarConfig = {
-      animated: true,
-      hidden: false,
-      showHideTransition: "fade",
-      backgroundColor: "rgba(0, 0, 0, 0.2)",
-      translucent: true,
+    const config = Object.assign({}, statusBarConfig, statusBar);
+
+    let statusBarStyles = [];
+    if (Platform.OS === 'ios') {
+        statusBarStyles = [styles.statusBarIOS, style];
+    } else if (Platform.OS === 'android') {
+        statusBarStyles = [styles.statusBarAndroid, style];
     }
-  }
-  const config = Object.assign({}, statusBarConfig, statusBar)
 
-  let statusBarStyles = []
-  if (Platform.OS === "ios") {
-    statusBarStyles = [styles.statusBarIOS, style]
-  } else if (Platform.OS === "android") {
-    statusBarStyles = [styles.statusBarAndroid, style]
-  }
-
-  return (
-    <View style={statusBarStyles}>
-      <StatusBar {...config} />
-    </View>
-  )
+    return (
+        <View style={statusBarStyles}>
+            <StatusBar {...config} />
+        </View>
+    );
 }
 
 StatusBarEnhanced.propTypes = {
-  statusBar: PropTypes.object,
-  style: ViewPropTypes.style,
-}
+    statusBar: PropTypes.object,
+    style: ViewPropTypes.style
+};
 
 StatusBarEnhanced.defaultProps = {
-  style: {},
-  statusBar: {},
-}
+    style: {},
+    statusBar: {}
+};
 
-export { StatusBarEnhanced }
+export { StatusBarEnhanced };
